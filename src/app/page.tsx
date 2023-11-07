38226914120097b8768da59b14af8ab0c75b35ea
TEERAPONG_K <oppoalien@gmail.com>
2023-11-07 12:59:00 -0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,164 @@
-import Image from 'next/image'
+"use client";
+import { Roboto } from "next/font/google";
+import Image from "next/image";
+import signUpPicMobile from "../../public/assets/images/illustration-sign-up-mobile.svg";
+import signUpPicDesktop from "../../public/assets/images/illustration-sign-up-desktop.svg";
+import listPic from "../../public/assets/images/icon-list.svg";
+import successPic from "../../public/assets/images/icon-success.svg";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useState } from "react";
+
+type Inputs = {
+  email: string;
+};
+
+const roboto = Roboto({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
 
 export default function Home() {
+  const [emailText, setEmailText] = useState("");
+  const [emailPassed, setEmailPassed] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    // watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+    setEmailPassed(true);
+  };
+
+  // console.log(watch("email"));
+
+  const handleInput = (value: string) => {
+    errors.email && reset();
+    setEmailText(value);
+  };
+
+  const backToFirstPage = () => {
+    setEmailPassed(false);
+    setEmailText("");
+    reset();
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main
+      className={`${roboto.className} w-[375px] md:w-[1440px] md:bg-[var(--Charcoal-Grey)]`}
+    >
+      {!emailPassed && (
+        <div className="h-[842px] md:h-[1080px] md:flex justify-center items-center">
+          <div className="md:flex flex-row-reverse bg-white md:rounded-[30px]">
+            <div className="imageBlock md:p-6">
+              <Image
+                className="md:hidden"
+                src={signUpPicMobile}
+                alt="illustration-sign-up-mobile"
+                priority
+              />
+              <Image
+                className="hidden md:block"
+                src={signUpPicDesktop}
+                alt="illustration-sign-up-mobile"
+                priority
+              />
+            </div>
+            <div className="contentBlock md:w-[480px] md:flex justify-center items-center p-6 md:p-10 md:pl-16">
+              <div className="flex flex-col gap-6">
+                <h1 className="text-[2.5em] md:text-[3.5em] font-bold">
+                  Stay updated!
+                </h1>
+                <p>
+                  Join 60,000+ product managers receiving monthly updates on:
+                </p>
+                <div className="listBlock flex flex-col gap-4">
+                  <div className="list flex items-start gap-4">
+                    <Image src={listPic} alt="icon-list" />
+                    <p>Product discovery and building what matters</p>
+                  </div>
+                  <div className="list flex items-start gap-4">
+                    <Image src={listPic} alt="icon-list" />
+                    <p>Measuring to ensure updates are a success</p>
+                  </div>
+                  <div className="list flex items-start gap-4">
+                    <Image src={listPic} alt="icon-list" />
+                    <p>And much more!</p>
+                  </div>
+                </div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <div className="w-full flex justify-between text-sm font-bold my-2">
+                    <label htmlFor="emailInput" className="">
+                      Email address
+                    </label>
+                    {errors.email && (
+                      <p className="text-sm text-[var(--Tomato)]">
+                        Valid email required
+                      </p>
+                    )}
+                  </div>
+
+                  <input
+                    type="text"
+                    placeholder="email@company.com"
+                    className={`w-full text-sm rounded-lg p-6 py-4 mb-4 cursor-pointer
+                ${
+                  !errors.email
+                    ? "border border-[var(--Grey)]"
+                    : "border-2 border-[var(--Tomato)] bg-red-100 text-[var(--Tomato)] font-bold"
+                }
+                `}
+                    {...register("email", {
+                      required: true,
+                      pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                    })}
+                    onChange={(e) => handleInput(e.target.value)}
+                    value={emailText}
+                  />
+                  <button
+                    type="submit"
+                    className="w-full rounded-lg bg-[var(--Dark-Slate-Grey)] text-white p-6 py-4
+                    hover:bg-gradient-to-r from-rose-500 to-[var(--Tomato)] hover:shadow-xl hover:shadow-rose-500/50
+                    "
+                  >
+                    Subscribe to monthly newsletter
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+      )}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      {emailPassed && (
+        <div className="h-[812px] md:h-[1080px] md:flex justify-center items-center">
+          <div className="md:w-[480px] h-full md:h-fit md:block flex flex-col justify-between p-6 py-10 md:p-14 md:bg-white md:rounded-[30px]">
+            <div className="py-24 md:p-0 md:pb-10">
+              <Image src={successPic} alt="icon-list" width={65} />
+              <h1 className="text-[2.5em] md:text-[3.5em] font-bold leading-10 md:leading-none my-10 mb-6">
+                Thanks for subscribing!
+              </h1>
+              <p>
+                A confirmation email has been sent to{" "}
+                <span className="font-bold">{emailText}</span>. Please open it
+                and click the button inside to confirm your subscription.
+              </p>
+            </div>
+            <div>
+              <button
+                onClick={() => backToFirstPage()}
+                className="w-full rounded-lg bg-[var(--Dark-Slate-Grey)] text-white p-6 py-4
+                hover:bg-gradient-to-r from-rose-500 to-[var(--Tomato)] hover:shadow-xl hover:shadow-rose-500/50
+                "
+              >
+                Dismiss message
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </main>
-  )
+  );
 }
